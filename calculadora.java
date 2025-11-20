043bdb3b5eadb688911e63c37ba41cfd502dc186
oscar <oscar@oscar>
2025-11-20 18:48:52 +0100

    Cambio para comprobar que pasa si hacemos pull.
    Cambio para ver que pasa si hacemos pull despues de hacer un commit en
    local.

--- calculadora.java
+++ calculadora.java
@@ -1,3 +1,4 @@
 //Archivo .java para tarea1 de entornos de desarrollo. Calculadora
 //Cambio para hacer el commit
 //Cambio que hago desde github para hacer pull desde local
+//Otro cambio para comprobar que pasa si despues de este intentamos bajar de nuevo de online
